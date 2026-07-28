92b7cba1de438e360374a4b091f5c4e7cb368e9c
Samridhi Khanna <130238405+SamridhiK2518@users.noreply.github.com>
2026-07-28 12:18:04 +0530

    Implement single number solution in C++

---
+++ CPP/136-Single-Number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        int result = nums[0];
+        for (int i = 1; i < n; i++) {
+            result = result ^ nums[i];
+        }
+        return result;  // ✅ Return the result
+    }
+};
